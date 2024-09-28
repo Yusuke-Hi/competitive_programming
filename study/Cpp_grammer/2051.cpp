716dec4f537693516026c2206b5a593c1ae6033e
yusuke <yuusuke29413@gmail.com>
2024-09-28 11:25:58 +0900

    self function

--- study/Cpp_grammer/2051.cpp
+++ study/Cpp_grammer/2051.cpp
@@ -1,14 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// x番の組織について、子組織からの報告書が揃った時刻を返す
+// x番の組織が親組織に提出する枚数を返す
 // childrenは組織の関係を表す2次元配列(参照渡し)
-int complete_time(vector<vector<int>> &children, int x) {
+int count_report_num(vector<vector<int>> &children, int x) {
   // (ここに追記して再帰関数を実装する)
-  if (){
-    return 0
+  if (children.at(x).size()==0){
+    return 1;
   }
-  x = children.at(x).
+  int result = 0;
+  for (int c: children.at(x)){
+    int child_num = count_report_num(children, c);
+    result += child_num;
+  }
+  result++;
+  return result;
 }
 
 // これ以降の行は変更しなくてよい
@@ -23,13 +29,15 @@ int main() {
     cin >> p.at(i);
   }
 
-  // 組織の関係から2次元配列を作る(理解しなくてもよい)
-  vector<vector<int>> children(N);  // ある組織の子組織の番号一覧  // N×0の二次元配列
+  // 組織の関係から2次元配列を作る
+  vector<vector<int>> children(N);  // ある組織の子組織の番号一覧
   for (int i = 1; i < N; i++) {
     int parent = p.at(i);  // i番の親組織の番号
     children.at(parent).push_back(i);  // parentの子組織一覧にi番を追加
   }
 
-  // 0番の組織の元に報告書が揃う時刻を求める
-  cout << complete_time(children, 0) << endl;
+  // 各組織について、答えを出力
+  for (int i = 0; i < N; i++) {
+    cout << count_report_num(children, i) << endl;
+  }
 }
--- study/Cpp_grammer/test.cpp
+++ study/Cpp_grammer/test.cpp
@@ -1,19 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-    int H, W;
-    cin >> H >> W;
-    vector<vector<char>> data(H, vector<char>(W));
-    for (int i=0; i<H; i++){
-        for (int j=0; j<W; j++){
-            cin >> data.at(i).at(j);
-        }
+    vector<int> data = {1,2,3,4};
+    int x = 0;
+    for (int i = 0; i < data.size(); i++){
+        x = max(x, data.at(i));
     }
-    cout << "---------------" << endl;
-    for (int i=0; i<H; i++){
-        for (int j=0; j<W; j++){
-            cout << data.at(i).at(j);
-        }
-        cout << endl;
-    }
+    cout << x << endl;
 }

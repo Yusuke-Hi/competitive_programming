1654b54acdddd9bfcf70717612561b74994aafed
yusuke <yuusuke29413@gmail.com>
2024-11-20 08:36:03 +0900

    half of all

--- study/Tessoku/third/test.cpp
+++ study/Tessoku/third/test.cpp
@@ -1,38 +1,25 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
- 
-long long N, K;
-long long A[100009];
-long long S[100009]; // 累積和
 
- 
-// A[l] から A[r] までの合計値
-long long sum(int l, int r) {
-	return S[r] - S[l - 1];
-}
- 
 int main() {
-    long long R[100009]; // 左端が決まったとき、右端はどこまで行けるか
-	// 入力
-	cin >> N >> K;
-	for (int i = 1; i <= N; i++) cin >> A[i];
- 
-	// 累積和をとる
-	S[0] = 0;
-	for (int i = 1; i <= N; i++) S[i] = S[i - 1] + A[i];
- 
-	// しゃくとり法
-	for (int i = 1; i <= N; i++) {
-		if (i == 1) R[i] = 0;
-		else R[i] = R[i - 1];
-		while (R[i] < N && sum(i, R[i] + 1) <= K) {
-			R[i] += 1;
-		}
-	}
-	
-	// 答えを求める
-	long long Answer = 0;
-	for (int i = 1; i <= N; i++) Answer += (R[i] - i + 1);
-	cout << Answer << endl;
-	return 0;
+    int N, K;
+    cin >> N >> K;
+    int A[N];
+    for (int i = 0; i < N; i++) cin >> A[i];
+    
+    int B[N * N];
+    for (int i = 0; i < (1 << N); i++){
+        cout << i << " ";
+        cout << endl;
+        bitset<15> bs(i);
+        int sum = 0;
+        for (int j = 0; j < N; j++){
+            if (bs[j]) sum += A[j];
+        }
+        B[i] = sum;
+    }
+    sort(B, B + (1 << N));
+
+    for (int i = 0; i < (1 << N); i++) cout << B[i] << " ";
+    cout << endl;
 }

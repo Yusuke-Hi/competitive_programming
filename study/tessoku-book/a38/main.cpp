cc7f3d9a1d059aa13f9cbc45fc1365ff7e63ac69
yusuke <yuusuke29413@gmail.com>
2024-12-16 09:03:07 +0900

    think

--- study/tessoku-book/a38/main.cpp
+++ study/tessoku-book/a38/main.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main() {
-    
+    int d, n;
+    cin >> d >> n;
+    int l[10009], r[10009], h[10009];
+    for (int role = 0; role < n; role++) cin >> l[role] >> r[role] >> h[role];
+
+    int lim[400];
+    for (int day = 1; day < d + 1; day++) lim[day] = 24;
+    for (int role = 0; role < n; role++){
+        for (int day = l[role]; day < r[role] + 1; day++){
+            lim[day] = min(lim[day], h[role]);
+        }
+    }
+    int ans = 0;
+    for (int day = 1; day < d + 1; day++) ans += lim[day];
+    cout << ans << endl;
 }

d63fbb8c266f683e3995d1b08718030c652a9746
yusuke <yuusuke29413@gmail.com>
2024-12-19 09:02:45 +0900

    think!

--- study/tessoku-book/b39/main.cpp
+++ study/tessoku-book/b39/main.cpp
@@ -3,20 +3,29 @@ using namespace std;
 
 int main() {
     int n, d;
-    int x[2009], y[1000009];
     cin >> n >> d;
-    for (int i = 1; i < n + 1; i++) cin >> x[i] >> y[i];
+    vector<pair<int, int>> jobs(n);
+    for (int i = 1; i < n + 1; i++) cin >> jobs[i].first >> jobs[i].second;
+    sort(jobs.begin(), jobs.end());
 
     vector<int> tmp;
     int current_t = 1;
     long long ans = 0;
+    //i = 0から
     for (int i = 1; i < n + 1; i++){
-        if (current_t < x[i]){
+        if (current_t < jobs[i].first && (!tmp.empty())){
             sort(tmp.begin(), tmp.end());
             ans += tmp.at(tmp.size() - 1);
             tmp.pop_back();
         }
-        tmp.push_back(y[i]);
+        tmp.push_back(jobs[i].second);
+        current_t = jobs[i].first;
+        if (i == n && jobs[i - 1].first < jobs[i].first){
+            sort(tmp.begin(), tmp.end());
+            ans += tmp.at(tmp.size() - 1);
+            tmp.pop_back();
+        }
     }
-    
+
+    cout << ans << endl;
 }

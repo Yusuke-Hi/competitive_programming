de66bc8cb97e5c6b1645d218c5df15cace2dbdd9
yusuke <yuusuke29413@gmail.com>
2024-12-23 07:07:44 +0900

    think

--- study/Tessoku/fourth/test.cpp
+++
@@ -1,8 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main() {
-    int a = 1, b = 2, c = 3;
-    int m = max({a, b, c});
-    cout << m << endl;
-}
---
+++ study/tessoku-book/b41/main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int X, Y;
+    cin >> X >> Y;
+
+    int x = 1, y = 1;
+    int cnt = 0;
+
+}

3a2bff6af29450c5b1402f527fb3ba48cef0e00b
yusuke <yuusuke29413@gmail.com>
2025-01-17 06:50:25 +0900

    dfs

--- study/tessoku-book/b62/main.cpp
+++ study/tessoku-book/b62/main.cpp
@@ -2,20 +2,37 @@
 #include <vector>
 #include <string>
 using namespace std;
-
+ 
+// 入力
 int N, M, A[100009], B[100009];
+ 
+// 深さ優先探索
 vector<int> G[100009];
-bool visited[100009]; // 頂点 x が青色の場合、visited[x] = true
-
-void dfs(int pos) { // pos は現在位置
+bool visited[100009];
+ 
+// 深さ優先探索の跡
+vector<int> Path, Answer;
+ 
+void dfs(int pos) {
+	// ゴール地点にたどり着いた！
+	if (pos == N) {
+		Answer = Path;
+		return;
+	}
+ 
+	// その他の場合
 	visited[pos] = true;
 	for (int i = 0; i < G[pos].size(); i++) {
 		int nex = G[pos][i];
-		if (visited[nex] == false) dfs(nex);
+		if (visited[nex] == false) {
+			Path.push_back(nex); // 頂点 nex を経路に追加
+			dfs(nex);
+			Path.pop_back(); // 頂点 nex を経路から削除
+		}
 	}
 	return;
 }
-
+ 
 int main() {
 	// 入力
 	cin >> N >> M;
@@ -24,25 +41,17 @@ int main() {
 		G[A[i]].push_back(B[i]);
 		G[B[i]].push_back(A[i]);
 	}
-
-    cout << "------------------" << endl;
-    for (int i = 1; i <= N; i++){
-        for (int j = 0; j <= G[i].size(); j++) cout << G[i][j] << " ";
-        cout << endl;
-    }
-
+ 
 	// 深さ優先探索
 	for (int i = 1; i <= N; i++) visited[i] = false;
+	Path.push_back(1); // 頂点 1（スタート地点）を経路に追加
 	dfs(1);
-
-    cout << "------------------" << endl;
-    for (int i = 1; i <= N; i++) cout << i << ":" << visited[i] << endl;
-
+ 
 	// 答えの出力
-	string Answer = "The graph is connected.";
-	for (int i = 1; i <= N; i++) {
-		if (visited[i] == false) Answer = "The graph is not connected.";
+	for (int i = 0; i < Answer.size(); i++) {
+		if (i >= 1) cout << " ";
+		cout << Answer[i];
 	}
-	cout << Answer << endl;
+	cout << endl;
 	return 0;
 }

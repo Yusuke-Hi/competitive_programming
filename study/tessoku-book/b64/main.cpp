97a4b85e41d3a25788efb83bea6ae878c9be98de
yusuke <yuusuke29413@gmail.com>
2025-01-22 06:47:44 +0900

    ok

---
+++ study/tessoku-book/b64/main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    int a[n + 1], b[n + 1], c[n + 1];
+    for (int i = 1; i <= m; i++){
+        cin >> a[i] >> b[i] >> c[i];
+    }
+    vector<int> graph[n + 1];
+}

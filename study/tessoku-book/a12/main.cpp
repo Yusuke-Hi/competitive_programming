aae7dee77f8b492515f1511589dcc5909f089327
yusuke <yuusuke29413@gmail.com>
2025-02-07 06:26:14 +0900

    anser bs

---
+++ study/tessoku-book/a12/main.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int check(int n, int s, int *a){
+    int total = 0;
+    for (int i = 1; i <= n; i++){
+        total += s / a[i];
+    }
+    return total;
+}
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+    int a[n + 1];
+    for (int i = 1; i <= n; i++) cin >> a[i];
+
+    int m;
+    int l = 1, r = 1000000000;
+    while(true){
+        m = (l + r) / 2;
+        if (check(n, m, a) < k){
+            l = m;
+        }
+        else{
+            r = m;
+        }
+
+        if (l + 1 == r){
+            cout << r << endl;
+            return 0;
+        }
+    }
+}

5b6998060902aa1b3b31876ab5e8910473b68207
yusuke <yuusuke29413@gmail.com>
2025-02-18 05:53:26 +0900

    dp

--- study/tessoku-book/a16/main.cpp
+++ study/tessoku-book/a16/main.cpp
@@ -5,15 +5,14 @@ int main() {
     int n;
     cin >> n;
     int a[n + 1], b[n + 1];
-    for (int i = 2; i < n + 1; i++) cin >> a[i];
-    for (int i = 3; i < n + 1; i++) cin >> b[i];
+    for (int i = 2; i <= n; i++) cin >> a[i];
+    for (int i = 3; i <= n; i++) cin >> b[i];
 
     int dp[n + 1];
     dp[1] = 0;
     dp[2] = a[2];
-    for (int i = 2; i < n + 1; i++){
-        dp[i + 1] = min(dp[i] + a[i + 1], )
-        dp[i + 2] = min(dp[i] + b[i + 2])
+    for (int i = 3; i <= n; i++){
+        dp[i] = min(dp[i - 2] + b[i], dp[i - 1] + a[i]);
     }
     cout << dp[n] << endl;
 }
